458801a3a2f8a106dbddeb60a8857ead9fcbb52d
MalikDanishRehman <danishrehman6069@gmail.com>
2025-12-02 10:28:43 -0800

    message

---
+++ AI_Driven_Water_Supply.Presentation/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿window.toggleSidePanel = function () {
+    let side = document.getElementById("sidePanel");
+    let body = document.querySelector(".dashboard-container");
+
+    if (side.classList.contains("side-panel-active")) {
+        side.classList.remove("side-panel-active");
+        body.classList.remove("body-shrink");
+    } else {
+        side.classList.add("side-panel-active");
+        body.classList.add("body-shrink");
+    }
+};
